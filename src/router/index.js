cc7762cabc0323844d0ee56d1877b34604f765dc
SuperArilo <you@example.com>
2022-04-02 17:10:48 +0800

    测试新的v5编辑器

--- src/router/index.js
+++ src/router/index.js
@@ -149,6 +149,14 @@ const routes = [
         }
     },
     {
+        path: '/test',
+        name: 'test',
+        component: () => import('@/views/test.vue'),
+        meta:{
+            title: 'test'
+        }
+    },
+    {
         path: '/:pathMatch(.*)',
         redirect: '/error'
     }
